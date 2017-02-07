d0946b2416d2dbd97f01e817b2cbf39b09f1fd69
Ulrich Raab <ulrich.raab@outlook.com>
2017-02-07 06:30:20 +0100

    Basic PassPhrase Generator
    generates a Passphrase of given length, including 2 numeric digits at
    the end. German words used.

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php
 
+function getPass($length) {
+
+$phraseList="Auto,Bahn,Zug,Hafen,Schiff,Boot,Depot,Holz,Farbe,Schrift,Name,Kopf,Hand,Blume,Wald,Baum,
+             Eisen,Kupfer,Metall,Rohr,Kind,Mutter,Vater,Torte,Geburtstag,Kerze";
+$phrases=explode(",", $phraseList);
 $alpha='abcdefghijklmnopqrstuvwxyz';
 $alphaUpper=strtoupper($alpha);
 $numeric='0123456789';
 $special=".-+=_,!@$#*%<>[]{}";
-$chars="";  
+$chars="";
 
+$randPhrases=array_rand($phrases,$length);
+$pw='';
+$len=strlen($numeric);
+
+foreach ($randPhrases as $phrase) {
+  $pw.=$phrases[$phrase];
+}
+$pw.=substr($numeric, rand(0, $len-1), 1);
+$pw.=substr($numeric, rand(0, $len-1), 1);
+
+return $pw;
+};
+
+echo getPass(2);
  ?>
