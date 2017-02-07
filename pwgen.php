1ff41971fae02378af27d01e3617ea576f719b92
Ulrich Raab <ulrich.raab@outlook.com>
2017-02-07 07:53:16 +0100

    clean ups

--- pwgen.php
+++ pwgen.php
@@ -4,7 +4,13 @@
 function getPass($wordCount, $minLength, $maxLength) {
 
 $phraseList="Auto,Bahn,Zug,Hafen,Schiff,Boot,Depot,Holz,Farbe,Schrift,Name,Kopf,Hand,Blume,Wald,Baum,
-             Eisen,Kupfer,Metall,Rohr,Kind,Mutter,Vater,Torte,Geburtstag,Kerze,Hyper,Mega,Kommando,Tatze,Katze,Ratte,Tat";
+             Eisen,Kupfer,Metall,Rohr,Kind,Mutter,Vater,Torte,Geburtstag,Kerze,Hyper,Mega,Kommando,Tatze,Katze,Ratte,Tat,Boje";
+##############
+# To-Dos:
+# Phrasenliste kodieren und in CSV-Datei abspeichern?
+# Danach einlesen und dekodieren.
+# ############
+
 $phrases=explode(",", $phraseList);
 $numeric='0123456789';
 $pw='';
@@ -25,5 +31,6 @@ $pw.=substr($numeric, rand(0, $len-1), 1);
 return $pw;
 };
 
-#echo getPass(2,8,4); // Example: get a Password of 2 Phrases, with a max passwort length of 8, and a minimum length of 4.
+
+#echo getPass(2,8,4); // Example: get a Password of 2 Phrases, with a max phrase length of 8, and a minimum length of 4.
  ?>
